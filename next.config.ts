f631003bcd112bca1d0e43d815faa0d5415121c8
Alex Lerman <alexander.a.lerman@gmail.com>
2025-07-18 11:55:29 -0700

    updating config for images

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,25 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  // replace `my-portfolio` with your repo name
+  const repo = 'personal-website'
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig: NextConfig = {
   output: 'export', // Required for GitHub Pages static export
   reactStrictMode: true,
   images: {
     unoptimized: true // Required for GitHub Pages
   },
-  basePath: "/personal-website",
+  basePath,
+  assetPrefix,
 };
 
 export default nextConfig;
